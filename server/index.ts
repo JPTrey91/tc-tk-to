da4275aadb14fc75bf5acb46417a05a24bccaedd
JPTrey <jptrey91@gmail.com>
2024-04-07 13:44:55 -0400

    BUGFIX: local testing through CORS impossible due to hard-coded `https`.

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const server = require("http").createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `https://${process.env.CLIENT_URL}`,
+    origin: `${process.env.CLIENT_URL}`,
   },
 });
 
